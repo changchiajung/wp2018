6042932f7d4f9e97aee43604bc44417127cb05bd
chiajung <rabbit20058@gmail.com>
2018-09-25 23:05:22 +0800

    work2

---
+++ profile.js
@@ -0,0 +1,14 @@
+/*
+function startweb()
+{
+  
+  var middle_frame=document.getElementById("middle_frame");
+  console.log(middle_frame);
+  var rect = middle_frame.getBoundingClientRect();
+  console.log(rect.top, rect.right, rect.bottom, rect.left);
+}
+*/
+function dis_middle_frame()
+{
+  document.getElementById("middle_frame").style.display="none";
+}
